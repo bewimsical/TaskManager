9ace6313c5f471c498e48540e5643b8477546914
Kathleen Wims <bewimsical@gmail.com>
2025-11-05 17:30:17 -0500

    Updated profile page

--- src/main/java/edu/farmingdale/taskmanager/Controllers/ProfileController.java
+++ src/main/java/edu/farmingdale/taskmanager/Controllers/ProfileController.java
@@ -1,4 +1,83 @@
 package edu.farmingdale.taskmanager.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+
 public class ProfileController {
+
+    @FXML
+    private Rectangle allButton;
+
+    @FXML
+    private Label allLabel;
+
+    @FXML
+    private StackPane bossesButton;
+
+    @FXML
+    private Label bossesLabel;
+
+    @FXML
+    private Label currentXp;
+
+    @FXML
+    private Label levelLabel;
+
+    @FXML
+    private Label levelXp;
+
+    @FXML
+    private ImageView profilePic;
+
+    @FXML
+    private StackPane questsButton;
+
+    @FXML
+    private Label questsLabel;
+
+    @FXML
+    private Label ritualsLabel;
+
+    @FXML
+    private Label username;
+
+    @FXML
+    private ProgressBar xpBar;
+
+    private Label[] buttons = {allLabel, ritualsLabel, questsLabel, bossesLabel};
+
+    @FXML
+    void showAll(MouseEvent event) {
+        setActiveButton(allLabel);
+    }
+
+    @FXML
+    void showBosses(MouseEvent event) {
+        setActiveButton(bossesLabel);
+    }
+
+    @FXML
+    void showQuests(MouseEvent event) {
+        setActiveButton(questsLabel);
+    }
+
+    @FXML
+    void showRituals(MouseEvent event) {
+        setActiveButton(ritualsLabel);
+    }
+
+    private void setActiveButton(Label button){
+        for(Label label:buttons){
+            label.getStyleClass().remove("active");
+        }
+
+        button.getStyleClass().add("active");
+    }
+
 }
+
