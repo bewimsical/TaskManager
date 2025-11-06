54650ee3cde808e804760f211f989553944dcdfc
Lena Kemp <lkemp023@gmail.com>
2025-11-06 13:18:04 -0500

    Added Bosses and Rituals Classes

---
+++ src/main/java/edu/farmingdale/taskmanager/Models/Bosses.java
@@ -0,0 +1,14 @@
+package edu.farmingdale.taskmanager.Models;
+
+import java.util.List;
+
+public class Bosses {
+    private String name;
+    private String attacks;
+    private int xp;
+    private double totalHealth;
+    private double currentHealth;
+    private List<String> chores;
+    private boolean bounties;
+    private boolean vanquished;
+}
---
+++ src/main/java/edu/farmingdale/taskmanager/Models/Ritual.java
@@ -0,0 +1,8 @@
+package edu.farmingdale.taskmanager.Models;
+
+import java.time.LocalDate;
+
+public class Ritual {
+    private LocalDate date;
+
+}
