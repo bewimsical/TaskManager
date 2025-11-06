d198483d9c060d798a353036fe140a40f12db637
Lena Kemp <lkemp023@gmail.com>
2025-11-06 13:29:54 -0500

    Update Bosses Class

--- src/main/java/edu/farmingdale/taskmanager/Models/Bosses.java
+++ src/main/java/edu/farmingdale/taskmanager/Models/Bosses.java
@@ -11,4 +11,84 @@ public class Bosses {
     private List<String> chores;
     private boolean bounties;
     private boolean vanquished;
+
+    public Bosses() {
+
+    }
+
+    public Bosses(String name, String attacks, int xp, double totalHealth, double currentHealth, List<String> chores, boolean bounties, boolean vanquished) {
+        this.name = name;
+        this.attacks = attacks;
+        this.xp = xp;
+        this.totalHealth = totalHealth;
+        this.currentHealth = currentHealth;
+        this.chores = chores;
+        this.bounties = bounties;
+        this.vanquished = vanquished;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAttacks() {
+        return attacks;
+    }
+
+    public void setAttacks(String attacks) {
+        this.attacks = attacks;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
+    public double getTotalHealth() {
+        return totalHealth;
+    }
+
+    public void setTotalHealth(double totalHealth) {
+        this.totalHealth = totalHealth;
+    }
+
+    public double getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public void setCurrentHealth(double currentHealth) {
+        this.currentHealth = currentHealth;
+    }
+
+    public List<String> getChores() {
+        return chores;
+    }
+
+    public void setChores(List<String> chores) {
+        this.chores = chores;
+    }
+
+    public boolean isBounties() {
+        return bounties;
+    }
+
+    public void setBounties(boolean bounties) {
+        this.bounties = bounties;
+    }
+
+    public boolean isVanquished() {
+        return vanquished;
+    }
+
+    public void setVanquished(boolean vanquished) {
+        this.vanquished = vanquished;
+    }
 }
