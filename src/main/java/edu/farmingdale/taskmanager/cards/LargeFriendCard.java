260c105c0d4a302c0ea93ef6240d8276da2e0084
Katie <bewimsical@gmail.com>
2025-11-07 23:05:10 -0500

    Updated map image in the quests view

---
+++ src/main/java/edu/farmingdale/taskmanager/cards/LargeFriendCard.java
@@ -0,0 +1,56 @@
+package edu.farmingdale.taskmanager.cards;
+
+import edu.farmingdale.taskmanager.Models.User;
+import edu.farmingdale.taskmanager.TaskManagerApplication;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class LargeFriendCard extends ImageCard<User>{
+    String username;
+
+    public LargeFriendCard(User data) {
+        super(data);
+        this.username = data.getUsername();
+    }
+
+
+    @Override
+    public void redraw() {
+
+    }
+
+    @Override
+    public Pane createView() {
+        Image image;
+        image = new Image(TaskManagerApplication.class.getResource("images/Player Card.png").toExternalForm());
+
+        ImageView imageview = new ImageView();
+        imageview.setFitHeight(862);
+        imageview.setFitWidth(267);
+        imageview.setImage(image);
+
+        Label name = new Label(this.username);
+        name.getStyleClass().add("small-friend-card-name");
+        name.setPadding(new Insets(8, 0, 10, 0));
+
+        Image profilePic;
+        profilePic = new Image(TaskManagerApplication.class.getResource("images/"+data.getProfileUrl()).toExternalForm());
+
+        ImageView imageview2 = new ImageView();
+        imageview2.setFitHeight(104);
+        imageview2.setFitWidth(104);
+        imageview2.setImage(profilePic);
+
+        root.setAlignment(Pos.CENTER);
+        VBox container = new VBox(name, imageview2);
+        container.setAlignment(Pos.TOP_CENTER);
+
+        root.getChildren().addAll(imageview, container);
+        return root;
+    }
+}
