67e36e774f25a154f8fc31308be11731dea0a057
deannee <morrdl@farmingdale.edu>
2025-11-08 22:35:01 -0500

    creating FXML for settings

--- src/main/java/edu/farmingdale/taskmanager/Controllers/SettingsController.java
+++ src/main/java/edu/farmingdale/taskmanager/Controllers/SettingsController.java
@@ -1,4 +1,26 @@
 package edu.farmingdale.taskmanager.Controllers;
 
+import javafx.event.ActionEvent;
+
 public class SettingsController {
+    public void handleProfileClick(ActionEvent event) {
+    }
+
+    public void handleSoundSettingsClick(ActionEvent actionEvent) {
+    }
+
+    public void handleParentalControlsClick(ActionEvent actionEvent) {
+    }
+
+    public void handleLogoutClick(ActionEvent actionEvent) {
+    }
+
+    public void handleGameSettingsClick(ActionEvent actionEvent) {
+    }
+
+    public void handleAccountSecurityClick(ActionEvent actionEvent) {
+    }
+
+    public void handleDeactivateAccountClick(ActionEvent actionEvent) {
+    }
 }
