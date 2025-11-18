55d48ccf5df5a215931eff8b9f15d1d3dedfcc7f
deannee <morrdl@farmingdale.edu>
2025-11-18 12:04:50 -0500

    creating FXML for sound

--- src/main/java/edu/farmingdale/taskmanager/Controllers/SoundSettingsController.java
+++ src/main/java/edu/farmingdale/taskmanager/Controllers/SoundSettingsController.java
@@ -1,6 +1,23 @@
 package edu.farmingdale.taskmanager.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+
 public class SoundSettingsController {
+
+    @FXML private Slider masterSlider;
+    @FXML private Slider musicSlider;
+    @FXML private Slider effectsSlider;
+
+    @FXML private Label masterValue;
+    @FXML private Label musicValue;
+    @FXML private Label effectsValue;
+
+    @FXML private CheckBox muteMusic;
+    @FXML private CheckBox muteEffects;
 
 
     }
