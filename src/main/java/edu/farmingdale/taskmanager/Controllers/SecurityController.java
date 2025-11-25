b9f4715db88e7d05da4c7fe86e3075d04c1a9187
deannee <morrdl@farmingdale.edu>
2025-11-25 16:38:13 -0500

    Security page update

---
+++ src/main/java/edu/farmingdale/taskmanager/Controllers/SecurityController.java
@@ -0,0 +1,28 @@
+package edu.farmingdale.taskmanager.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
+
+public class SecurityController {
+
+
+    public StackPane securityContentArea;
+    public Label statusLabel;
+    public Button btnChangePassword;
+    public Button btnChangeEmail;
+    public Button btnTwoFactor;
+    public Button btnDeviceSecurity;
+
+    public void openChangePassword(ActionEvent actionEvent) {
+    }
+
+    public void openChangeEmail(ActionEvent actionEvent) {
+    }
+
+    public void openDeviceSecurity(ActionEvent actionEvent) {
+    }
+
+    public void openTwoFactor(ActionEvent actionEvent) {
+    }
+}
