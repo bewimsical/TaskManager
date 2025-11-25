8df76ac8e572d796150aa5ef0971e4e26b809e07
Kathleen Wims <bewimsical@gmail.com>
2025-11-25 16:11:23 -0500

    converted bosses page to MVVM pattern

--- src/main/java/edu/farmingdale/taskmanager/viewmodels/BossViewModel.java
+++ src/main/java/edu/farmingdale/taskmanager/viewmodels/BossViewModel.java
@@ -4,10 +4,12 @@ import edu.farmingdale.taskmanager.Models.*;
 import edu.farmingdale.taskmanager.Repositories.FirebaseBossRepository;
 import edu.farmingdale.taskmanager.Repositories.FirebaseUserRepository;
 import edu.farmingdale.taskmanager.Session;
+import edu.farmingdale.taskmanager.TaskManagerApplication;
 import edu.farmingdale.taskmanager.cards.ChoreCard;
 import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
 
 import java.util.List;
 import java.util.Map;
@@ -19,15 +21,19 @@ public class BossViewModel {
 
     //setup property bindings
     private final StringProperty name = new SimpleStringProperty();
-    private final DoubleProperty xpPercent = new SimpleDoubleProperty();
+    private final DoubleProperty healthPercent = new SimpleDoubleProperty();
     private final ObjectProperty<Boss> selectedBoss = new SimpleObjectProperty<>();
+    private final BooleanProperty vanquished = new SimpleBooleanProperty();
     private final ObservableList<Boss> visibleBosses = FXCollections.observableArrayList();
     private final ObservableList<Chore> visibleChores = FXCollections.observableArrayList();
+    private final ObjectProperty<Image> bossImage = new SimpleObjectProperty<>();
 
-    //do i make this  a simple object property
+    //do I make this  a simple object property
     private ChoreCard<Boss> currentBossCard;
     private double totalHealth;
     private double currentHealth;
+    private Image aliveImage;
+    private Image deadImage;
 
     //store data from database
     private final Map<String, List<Boss>> bosses;
@@ -37,6 +43,24 @@ public class BossViewModel {
         Session session = Session.getInstance();
         user = session.getUser();
         bosses = session.getBosses();
+
+        selectedBoss.addListener((obs, oldBoss, newBoss) -> {
+            if (newBoss != null) {
+                vanquished.set(newBoss.isVanquished());
+                setUpImages(newBoss);
+                updateImage();
+            }
+        });
+
+        // When defeated property changes
+        vanquished.addListener((obs, oldVal, newVal) -> {
+            Boss b = selectedBoss.get();
+            if (b != null) {
+                b.setVanquished(newVal);
+                setUpImages(b);
+            }
+            updateImage();
+        });
     }
 
     public void setUpView(){
@@ -47,9 +71,63 @@ public class BossViewModel {
             visibleChores.clear();
             visibleChores.addAll(getSelectedBoss().getChores());
             setupHealthBar();
-
         }
     }
+
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public ObservableList<Boss> getVisibleBosses() {
+        return visibleBosses;
+    }
+
+    public ObservableList<Chore> getVisibleChores() {
+        return visibleChores;
+    }
+
+    public ObjectProperty<Boss> selectedQuestProperty() { return selectedBoss; }
+    public Boss getSelectedBoss() { return selectedBoss.get(); }
+    public void setSelectedBoss(Boss boss) { selectedBoss.set(boss); }
+
+    public ChoreCard<Boss> getCurrentBossCard() {
+        return currentBossCard;
+    }
+
+    public void setCurrentBossCard(ChoreCard<Boss> currentQuestCard) {
+        this.currentBossCard = currentQuestCard;
+    }
+
+    public double getHealthPercent() {
+        return healthPercent.get();
+    }
+
+    public DoubleProperty healthPercentProperty() {
+        return healthPercent;
+    }
+
+    public boolean isVanquished() {
+        return vanquished.get();
+    }
+
+    public BooleanProperty vanquishedProperty() {
+        return vanquished;
+    }
+
+    public Image getBossImage() {
+        return bossImage.get();
+    }
+
+    public ObjectProperty<Image> bossImageProperty() {
+        return bossImage;
+    }
+
+    public void setUpImages(Boss b){
+        aliveImage = new Image(TaskManagerApplication.class.getResource("images/monsters/" + b.getDirtyImageUrl()).toExternalForm());
+        deadImage = new Image(TaskManagerApplication.class.getResource("images/monsters/" + b.getCleanImageUrl()).toExternalForm());
+    }
+
     private void setupHealthBar(){
 //        totalHealth = visibleChores.stream()
 //                .mapToInt(Chore::getChoreXP)
@@ -62,40 +140,19 @@ public class BossViewModel {
 //                .sum();
         currentHealth = getSelectedBoss().getCurrentHealth();
 
-        xpPercent.set(currentHealth / totalHealth);
+        healthPercent.set(currentHealth / totalHealth);
     }
-
-    public StringProperty nameProperty() {
-        return name;
-    }
-
-    public ObservableList<Boss> getVisibleQuests() {
-        return visibleBosses;
-    }
-
-    public ObservableList<Chore> getVisibleChores() {
-        return visibleChores;
-    }
-
-    public ObjectProperty<Boss> selectedQuestProperty() { return selectedBoss; }
-    public Boss getSelectedBoss() { return selectedBoss.get(); }
-    public void setSelectedQuest(Boss boss) { selectedBoss.set(boss); }
-
-    public ChoreCard<Boss> getCurrentQuestCard() {
-        return currentBossCard;
-    }
-
-    public void setCurrentQuestCard(ChoreCard<Boss> currentQuestCard) {
-        this.currentBossCard = currentQuestCard;
+    private void updateImage() {
+        bossImage.set(vanquished.get() ? deadImage : aliveImage);
     }
 
     public void cardClick(ChoreCard<Boss> card){
         Boss boss = card.getData();
-        setSelectedQuest(boss);
+        setSelectedBoss(boss);
         name.set(boss.getName());
         visibleChores.clear();
         visibleChores.addAll(boss.getChores());
-        setCurrentQuestCard(card);
+        setCurrentBossCard(card);
         setupHealthBar();
 
 
@@ -131,15 +188,15 @@ public class BossViewModel {
                 .allMatch(Chore::isCompleted);
 
         if(bossCompleted){
-            currentBoss.setVanquished(true);
+            //currentBoss.setVanquished(true);
+            vanquished.set(true);
+            System.out.println("boss defeated??? "+currentBoss.isVanquished());
             bosses.get("Vanquished").add(currentBoss);
-            getCurrentQuestCard().redraw();
+            getCurrentBossCard().redraw();
 
         }
 
         card.redraw();
 
     }
-
-
 }
