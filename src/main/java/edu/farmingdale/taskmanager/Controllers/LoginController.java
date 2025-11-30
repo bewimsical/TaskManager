e11dc62bc6db56345176d965ea7aebbc57280355
Lena Kemp <lkemp023@gmail.com>
2025-11-30 15:39:48 -0500

    Styled Signup Page

--- src/main/java/edu/farmingdale/taskmanager/Controllers/LoginController.java
+++ src/main/java/edu/farmingdale/taskmanager/Controllers/LoginController.java
@@ -1,4 +1,24 @@
 package edu.farmingdale.taskmanager.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class LoginController {
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button googleButton;
+
 }
